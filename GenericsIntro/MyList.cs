5dfeeda9424e85acbe6e71f72b5acbf1fca3e973
FIRAT GÜLTEKİN <20184703040@ogr.akdeniz.edu.tr>
2021-05-14 13:20:18 +0300

    4. dersin sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+
+        public MyList()// Constructor. Bir yerde snıf yenilenirse bu blok otomatik çalışır.
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)// string türünde bir eleman ver demiş olduk.
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];// Dizinin eleman sayısı 1 arttırıldı.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;  
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ID = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 50;
+            product1.UnitsInStock = 10;
+
+            Product product2 = new Product { ID = 2, CategoryId = 5, ProductName = "Kalem", UnitPrice = 35, UnitsInStock = 28 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            
+
+
+        }
+    }
+}
